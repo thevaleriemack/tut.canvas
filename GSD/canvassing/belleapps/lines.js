91e57b87b976db572b65517a14d5d3be2defa5b3
valem21 <dgm97@cornell.edu>
2014-11-30 00:44:28 -0500

    Create connector fn to draw in lines.js

--- GSD/canvassing/belleapps/lines.js
+++ GSD/canvassing/belleapps/lines.js
@@ -1,12 +1,50 @@
-var canvas = document.getElementById('game');
+var canvas = document.getElementById('lines');
 var context = canvas.getContext('2d');
 
 context.beginPath();
+var w = window.innerWidth;
+var h = window.innerHeight;
+canvas.width = w;
+canvas.height = h;
 
-context.moveTo(20, 20);
-context.lineTo(20, 50);
+var m = 20;
+var n = 20;
 
-context.strokeStyle = '#cccff1';
+function save(a, b) {
+    // saves numbers set for a and b
+    m = a
+    n = b
+}
+
+function connector(x, y) {
+    // x and y are numbers
+    context.moveTo(m, n);
+    context.lineTo(x, y);
+    save(x, y);
+}
+
+// h1
+connector(100,20);
+
+// v1
+connector(100,100);
+
+// h2
+connector(40, 100);
+
+// v2
+connector(40, 200);
+
+// h3
+connector(100, 200);
+
+// v3
+connector(100, 400);
+
+// h4
+connector(20, 400);
+
+context.strokeStyle = '#000000';
 context.lineWidth = 3;
 context.lineCap = 'round';
 context.stroke();
